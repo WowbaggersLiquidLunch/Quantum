13618308563735f9f6111458554e87d081cc5f7b
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2021-01-15 12:39:50 -0500

    use tab after comment slashes

--- Sources/Quantum/Quantum.swift
+++ Sources/Quantum/Quantum.swift
@@ -6,12 +6,12 @@
 //	Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
 //
 
-/// A quantum mechanical model of the wrapped value.
+///	A quantum mechanical model of the wrapped value.
 @propertyWrapper
 public struct Quantum<Wrapped: Hashable> {
 	
-	/// Creates a quantum mechanical system with the given initial state.
-	/// - Parameter wrappedValue: The initial state to create a quantum mechanical system with.
+	///	Creates a quantum mechanical system with the given initial state.
+	///	- Parameter wrappedValue: The initial state to create a quantum mechanical system with.
 	public init(wrappedValue: Wrapped) {
 		quantumState = [wrappedValue: 1]
 	}
@@ -23,12 +23,12 @@ public struct Quantum<Wrapped: Hashable> {
 	
 	public var projectedValue: Self { self }
 	
-	/// The probability distribution for the outcomes of each possible measurement on the system.
+	///	The probability distribution for the outcomes of each possible measurement on the system.
 	public private(set) var quantumState: [Wrapped: Double]
 	
-	/// The observed state the system is in.
+	///	The observed state the system is in.
 	///
-	/// Measuring the outcome collapses the wave function.
+	///	Measuring the outcome collapses the wave function.
 	public var measurement: Wrapped {
 		mutating get {
 			collapseWaveFunction()
@@ -42,7 +42,7 @@ public struct Quantum<Wrapped: Hashable> {
 		}
 	}
 	
-	/// Reduces the wave function to a single eigenstate.
+	///	Reduces the wave function to a single eigenstate.
 	private mutating func collapseWaveFunction() {
 		var cumulativeProbability = Double.random(in: 0...1)
 		//	FIXME: There is probably a standard library function that does this.
@@ -55,16 +55,16 @@ public struct Quantum<Wrapped: Hashable> {
 		}
 	}
 	
-	/// Adds the given state to the superposition.
-	/// - Parameter state: The state to add to the superposition.
+	///	Adds the given state to the superposition.
+	///	- Parameter state: The state to add to the superposition.
 	public mutating func superpose(on state: Wrapped) {
 		quantumState = quantumState.mapValues { $0 / 2 }
 		quantumState[state, default: 0] += 0.5
 	}
 	
-	/// Creates a new quantum mechanical system with the superposition of the given state and a copy of the superposition of this system.
-	/// - Parameter state: The state to superpose on the copy of the superposition of this system.
-	/// - Returns: A new quantum mechanical system with the superposition of the given state and a copy of the superposition of this system.
+	///	Creates a new quantum mechanical system with the superposition of the given state and a copy of the superposition of this system.
+	///	- Parameter state: The state to superpose on the copy of the superposition of this system.
+	///	- Returns: A new quantum mechanical system with the superposition of the given state and a copy of the superposition of this system.
 	@inlinable
 	public func superposed(on state: Wrapped) -> Self {
 		var newSuperposition = self
